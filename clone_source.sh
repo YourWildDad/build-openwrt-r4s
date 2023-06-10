e4440155938b769b51c7b2ed8a0625e61aa3f983
YourWildDad <053700@gmail.com>
2023-06-10 12:09:06 +0800

    feat: r4s 更换回 5.4 内核

--- clone_source.sh
+++ clone_source.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-git clone --depth=1 --branch "master" --single-branch "https://github.com/immortalwrt/immortalwrt.git" /home/runner/openwrt
+git clone --depth=1 --branch "openwrt-21.02" --single-branch "https://github.com/immortalwrt/immortalwrt.git" /home/runner/openwrt
 cd /home/runner/openwrt
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
