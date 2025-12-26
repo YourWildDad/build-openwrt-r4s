8d2eda6879e6903894fc02bac118357844decbb5
YourWildDad <053700@gmail.com>
2025-12-26 11:19:59 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-git clone --depth=1 --branch "openwrt-23.05" --single-branch "https://github.com/immortalwrt/immortalwrt.git" /home/runner/openwrt
+git clone --depth=1 --branch "openwrt-25.12" --single-branch "https://github.com/immortalwrt/immortalwrt.git" /home/runner/openwrt
 cd /home/runner/openwrt
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
